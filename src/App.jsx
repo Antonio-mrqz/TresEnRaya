a091d449c3ee8afed43bd03e8ff60cbc50d4b18d
Antonio.mrqz_ <m.b.antonio898@gmail.com>
2024-09-10 13:43:15 +0200

    LogicaGanador
    Definimos la lógica para poder diferenciar cuando uno de los jugadores ha ganado.

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,62 @@ const Square = ({ children, isSelected, updateBoard, index }) =>{
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
+  // Inicializamos el tablero vacio y el primer turno
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
 
+  // Variable para saber el ganador, null = no hay ganador, false = empate
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck)=>{
+    // Revisamos todas las combinaciones posibles para ganar
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] && // Comprueba si en la primera hay algo (x u o)
+        boardToCheck[a] === boardToCheck[b] && // Comprueba que la primera es igual a la segunda
+        boardToCheck[a] === boardToCheck[c]  // Comprueba que la primera es igual a la tercera
+      ){
+        return boardToCheck[a] // x u o
+      }
+    }
+    // Si no hay ganador
+    return null
+
+  }
+
   const updateBoard = (index) =>{
+    //Comprueba si hay algo ya en el cuadrado para no sobreescribir
+    if (board[index] || winner) return;
+
+    //Guardamos el nuevo tablero en una variable y modificamos el original
     const newBoard = [ ... board]
     newBoard[index] = turn
     setBoard(newBoard);
+    
 
-
+    //Cambiamos de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    //Revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if(newWinner){
+      setWinner(newWinner);
+    }
+    //To do: Check game over
   }
   return (
     <>
@@ -51,6 +95,7 @@ function App() {
           })
         }
       </section>
+
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
@@ -59,6 +104,30 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+      winner != null && (
+        <section className='winner'>
+          <div className='text'>
+            <h2>
+              {
+                winner == false 
+                ? "Empate"
+                : "Ganó: "
+              }
+            </h2>
+
+            <header className='win'>
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button>Empezar de Nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )
+      }
     </main>
     </>
   )
